9503c48bdc04ae169b4f5c1ffa68b0dfc6db1dac
tommy lin <yu_dady@yahoo.com.tw>
2012-10-19 15:41:46 +0800

    fix error

--- src/main/java/com/tommy/demo/controller/MenuBarController.java
+++ src/main/java/com/tommy/demo/controller/MenuBarController.java
@@ -1,9 +1,7 @@
 package com.tommy.demo.controller;
 
 import java.io.IOException;
-import java.net.URL;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +16,7 @@ public class MenuBarController {
 
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody String getMenuBar() throws IOException {
-		logger.debug("[LOG]MenuBarController.getMenuBar()");
-		URL url = this.getClass().getClassLoader().getResource("/data/simpletree.txt");
-		logger.debug("[LOG][url]="+url);
-		String menu = IOUtils.toString(url);
-		return menu;
+		return "Menu <br/> in controller <br/> data";
 				
 	}
 
